8f291a4494671f8dabcc897485dcac110a6d5ead
Joseph Serunjogi <joeseggie@gmail.com>
2019-08-22 16:48:27 +0300

    Account balance

--- account/models.py
+++ account/models.py
@@ -10,6 +10,9 @@ class Account(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     account_number = models.CharField(max_length=20, unique=True)
+    balance = models.DecimalField(max_digits=10,
+                                  decimal_places=2,
+                                  default=0.00)
 
     def __str__(self):
         return self.account_number
